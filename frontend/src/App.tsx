1d0c679a4de8e53f1fbee0a032704d4a2a06cde4
Rajil Bajracharya <imrb1213@gmail.com>
2023-02-13 00:13:38 +0545

    Use loader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,26 +8,27 @@ import EventDetail from "./components/EventDetail";
 import NewEvent from "./components/NewEvent";
 import EditEvent from "./components/EditEvent";
 import EventsRootLayout from "./components/EventsRootLayout";
+import { Event } from "./components/EventItem";
 
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+// // 1. Add five new (dummy) page components (content can be simple <h1> elements)
+// //    - HomePage
+////    - EventsPage
+////    - EventDetailPage
+////    - NewEventPage
+////    - EditEventPage
+//// 2. Add routing & route definitions for these five pages
+////    - / => HomePage
+////    - /events => EventsPage
+////    - /events/<some-id> => EventDetailPage
+////    - /events/new => NewEventPage
+////    - /events/<some-id>/edit => EditEventPage
+//// 3. Add a root layout that adds the <MainNavigation> component above all page components
+//// 4. Add properly working links to the MainNavigation
+//// 5. Ensure that the links in MainNavigation receive an "active" class when active
+//// 6. Output a list of dummy events to the EventsPage
+////    Every list item should include a link to the respective EventDetailPage
+//// 7. Output the ID of the selected event on the EventDetailPage
+//// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
 const router = createBrowserRouter([
   {
@@ -39,7 +40,22 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <Events /> },
+          {
+            index: true,
+            element: <Events />,
+            loader: async () => {
+              const response = await fetch("http://localhost:8080/events");
+
+              if (!response.ok) {
+                // later
+              } else {
+                const resData = await response.json();
+
+                if (isEvents(resData)) return resData.events;
+                return {};
+              }
+            },
+          },
           { path: ":eventId", element: <EventDetail /> },
           { path: "new", element: <NewEvent /> },
           { path: ":eventId/edit", element: <EditEvent /> },
@@ -54,3 +70,9 @@ function App() {
 }
 
 export default App;
+
+// typeguard for events API
+const isEvents = (object: unknown): object is { events: Event[] } => {
+  if (object !== null && typeof object === "object") return "events" in object;
+  return false;
+};
--- frontend/src/components/Events.tsx
+++ frontend/src/components/Events.tsx
@@ -1,36 +1,13 @@
-import { useEffect, useState } from "react";
+import EventsList from "../components/EventsList";
+import { useLoaderData } from "react-router-dom";
 import { Event } from "./EventItem";
 
-import EventsList from "../components/EventsList";
-
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [fetchedEvents, setFetchedEvents] = useState<Event[]>([]);
-  const [error, setError] = useState<string>("");
+  const fetchedEvents = useLoaderData() as Event[];
 
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/events");
-
-      if (!response.ok) {
-        setError("Fetching events failed.");
-      } else {
-        const resData = await response.json();
-        setFetchedEvents(resData.events);
-      }
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
   return (
     <>
-      <div style={{ textAlign: "center" }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      <EventsList events={fetchedEvents} />
     </>
   );
 }
