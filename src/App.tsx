2829582c2a9e0bf7df8af21f9db87a2354049d1b
Rajil Bajracharya <imrb1213@gmail.com>
2023-02-19 23:46:51 +0545

    Load Blog Page lazily

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import BlogPage, { loader as postsLoader } from "./pages/Blog";
+// import BlogPage, { loader as postsLoader } from "./pages/Blog";
 import HomePage from "./pages/Home";
 import PostPage, { loader as postLoader } from "./pages/Post";
 import RootLayout from "./pages/Root";
+
+const BlogPage = lazy(() => import("./pages/Blog"));
 
 const router = createBrowserRouter([
   {
@@ -18,7 +20,16 @@ const router = createBrowserRouter([
       {
         path: "posts",
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>
+            ),
+            loader: (meta) =>
+              import("./pages/Blog").then((module) => module.loader(meta)),
+          },
           { path: ":id", element: <PostPage />, loader: postLoader },
         ],
       },
