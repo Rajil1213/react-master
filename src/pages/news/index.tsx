f49ff887989a532ea30629c2243384e25d34b67b
Rajil Bajracharya <imrb1213@gmail.com>
2023-02-26 01:00:30 +0545

    Add Links

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 export const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/1">News 1</Link>
+        </li>
+        <li>
+          <Link href="/news/2">News 2</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
