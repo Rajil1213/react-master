f6893da71cc2e7a29d3e20310791c55def638d3f
Rajil Bajracharya <imrb1213@gmail.com>
2023-02-26 23:55:05 +0545

    Use new-meetup api to create a new meetup

--- pages/new-meetup/index.tsx
+++ pages/new-meetup/index.tsx
@@ -1,9 +1,25 @@
 import { MeetupItemProps } from "@/components/meetups/MeetupItem";
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (enteredMeetupData: Partial<MeetupItemProps>) => {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  const addMeetupHandler = async (
+    enteredMeetupData: Partial<MeetupItemProps>
+  ) => {
+    const result = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await result.json();
+    console.log(data);
+
+    router.push("/");
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
