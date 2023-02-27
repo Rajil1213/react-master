922ee6940836e0894ac4a9cae5d209c0b8e6f625
Rajil Bajracharya <imrb1213@gmail.com>
2023-02-27 13:28:28 +0545

    Use classes object

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-// import classes from "./Modal.module.css";
-import "./Modal.css";
+import classes from "./Modal.module.css";
+// import "./Modal.css";
 import { CSSTransition } from "react-transition-group";
 
 interface ModalProps {
@@ -10,8 +10,21 @@ interface ModalProps {
 
 const Modal = (props: ModalProps) => {
   return (
-    <CSSTransition in={props.show} timeout={500} unmountOnExit mountOnEnter classNames="fade-slide">
-      <div className="Modal">
+    <CSSTransition
+      in={props.show}
+      timeout={500}
+      unmountOnExit
+      mountOnEnter
+      classNames={{
+        enter: "",
+        enterActive: classes.ModalOpen,
+        exit: "",
+        exitActive: classes.ModalClosed,
+        appear: "",
+        appearActive: "",
+      }}
+    >
+      <div className={classes.Modal}>
         <h1>A Modal</h1>
         <button className="Button" onClick={props.closed}>
           Dismiss
