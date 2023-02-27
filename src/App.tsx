4631592127a57b47c2384c195aa0944fe37c8308
Rajil Bajracharya <imrb1213@gmail.com>
2023-02-27 12:27:26 +0545

    Add dummy div to demo react-transition-group

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
+import Transition, { TransitionStatus } from "react-transition-group/Transition";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 function App() {
   const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+  const [showBlock, setShowBlock] = useState<boolean>(false);
 
   const showModal = () => {
     setModalIsOpen(true);
@@ -18,15 +20,31 @@ function App() {
   return (
     <div className="App">
       <h1>React Animations</h1>
-      <Modal
-        closed={closeModal}
-        show={modalIsOpen}
-      />
-      <Backdrop show={modalIsOpen} />
       <button
-        className="Button"
-        onClick={showModal}
+        onClick={() => {
+          setShowBlock((prevShowBlock) => !prevShowBlock);
+        }}
       >
+        Toggle
+      </button>
+      <br />
+      <Transition in={showBlock} timeout={500} mountOnEnter unmountOnExit>
+        {(state: TransitionStatus) => (
+          <div
+            style={{
+              backgroundColor: "red",
+              width: 100,
+              height: 100,
+              margin: "auto",
+              opacity: state === "exiting" ? 0 : 1,
+              transition: "opacity 1s ease-out",
+            }}
+          ></div>
+        )}
+      </Transition>
+      <Modal closed={closeModal} show={modalIsOpen} />
+      <Backdrop show={modalIsOpen} />
+      <button className="Button" onClick={showModal}>
         Open Modal
       </button>
       <h3>Animating Lists</h3>
